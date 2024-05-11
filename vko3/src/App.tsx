9ff2ef096bac98e2fedc7613bc24424f8b024339
Niclas Nilsson <81629906+Nilssoni56@users.noreply.github.com>
2024-05-11 14:13:08 +0300

    updated

--- vko3/src/App.tsx
+++ vko3/src/App.tsx
@@ -8,14 +8,10 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="https://nilssoni56.github.io/repo-pilvi/vko3/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/repo-pilvi/vko3/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img
-            src="https://nilssoni56.github.io/repo-pilvi/vko3/assets/react-CHdo91hT.svg"
-            className="logo react"
-            alt="React logo"
-          />
+          <img src="/repo-pilvi/vko3/assets/react-CHdo91hT.svg" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
